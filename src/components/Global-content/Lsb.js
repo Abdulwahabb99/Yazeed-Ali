4d3e2ecc09386775a138495088e4b759561c03e4
abdulwahab <abdulwahabmahmoud99@gmail.com>
2026-03-03 04:04:46 +0200

    Add Babel loader cache files for Lsb component updates, including layout adjustments and profile image enhancements.

--- src/components/Global-content/Lsb.js
+++ src/components/Global-content/Lsb.js
@@ -3,21 +3,13 @@ import React from "react";
 export default function Lsb() {
   return (
     <div className="left-sidebar">
-      {/* <div className=" d-flex flex-column align-items-center justify-content-center"> */}
-      <img
-        src="./assets/images/logo-yazeed.png"
-        alt="Logo"
-        style={{
-          width: "90%",
-          maxWidth: 450,
-        }}
-      />
-      {/* </div> */}
-
       <img className="me" src="./assets/images/yazeed-pic.jpeg" alt="Me" />
+      <div className="designation-wrap">
+        <span className="designation-badge">Senior Software Engineer</span>
+      </div>
       <h2 className="email">engyazeedali@gmail.com</h2>
       <h2 className="address">Tuwaiq Dist, Riyadh, Saudi Arabia</h2>
-      <p className="copyright">&copy; 2025 Yazeed Ali. All Rights Reserved</p>
+      {/* <p className="copyright">&copy; 2025 Yazeed Ali. All Rights Reserved</p> */}
       <ul className="social-profile d-flex align-items-center flex-wrap justify-content-center">
         <li>
           <a
@@ -38,9 +30,6 @@ export default function Lsb() {
           </a>
         </li>
       </ul>
-      <a href="#contact" className="theme-btn">
-        <i className="las la-envelope"></i> Hire Me!
-      </a>
     </div>
   );
 }
